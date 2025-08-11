657f73108590a56ae0670034d54b53dc664b2605
Stefan Prvulovic <prvulovic.stefan@elfak.rs>
2025-08-11 12:14:53 +0200

    refinement

--- src/tools/index.ts
+++ src/tools/index.ts
@@ -1,14 +1,12 @@
 import { ShellTool } from './shell';
 import { ReadTool } from './read';
 import { WriteTool } from './write';
-import { GrepTool } from './grep';
-import { SearchTool } from './search';
+import { SearchTool } from './search-files';
 import { Tool } from './types';
 
 export const availableTools: Tool[] = [
   new ShellTool(),
   new WriteTool(),
   new ReadTool(),
-  // new GrepTool(),
   new SearchTool(),
 ];
