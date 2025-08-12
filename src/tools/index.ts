c077b2dfb7e0475aafde9e6a33f7e496f609f57d
Stefan Prvulovic <prvulovic.stefan@elfak.rs>
2025-08-12 16:02:04 +0200

    feat(CG-65): Ls tool created

--- src/tools/index.ts
+++ src/tools/index.ts
@@ -3,12 +3,14 @@ import { ReadTool } from './read';
 import { WriteTool } from './write';
 import { SearchTool } from './search-files';
 import { Tool } from './types';
+import { LsTool } from './ls';
 
 export const availableTools: Tool[] = [
   new ShellTool(),
   new WriteTool(),
   new ReadTool(),
   new SearchTool(),
+  new LsTool(),
 ];
 
 export { Tool };
